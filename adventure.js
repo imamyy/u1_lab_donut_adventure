5b0b63c41809d34dcf42394bf3ea8dc0f91e1401
imamyy <amytran2608@gmail.com>
2022-08-20 16:38:02 -0400

    Dougie and Pizza Rat have fought

--- adventure.js
+++ adventure.js
@@ -12,13 +12,27 @@ class Hero {
     ];
   }
   talkSass() {
-    console.log(this.catchPhrases[Math.random() * 2]);
+    console.log(this.catchPhrases[Math.floor(Math.random() * 2)]);
   }
   announceHealth() {
     console.log(this.health);
   }
-  battle() {
+  battle(enemy) {
     console.log("I'm ready to rumble");
+    const attack =
+      this.objects[Object.keys(this.objects)[Math.floor(Math.random() * 2)]];
+    console.log(attack);
+    if (attack === 5) {
+      enemy.health -= 5;
+      console.log(
+        `PizzaRat got hit by sprinkleSpray! His health is now at ${enemy.health}`
+      );
+    } else if (attack === 10) {
+      enemy.health -= 10;
+      console.log(
+        `PizzaRat got hit by sugarShock! His health is now at ${enemy.health}`
+      );
+    }
   }
 }
 const donut = new Hero("Dougie");
@@ -37,13 +51,36 @@ class Enemy {
     ];
   }
   talkSmack() {
-    console.log(this.catchPhrases[Math.random() * 2]);
+    console.log(this.catchPhrases[Math.floor(Math.random() * 2)]);
   }
   announceHealth() {
     console.log(this.health);
   }
-  battle() {
+  battle(enemy) {
     console.log("i'm gonna flatten you like a slice of pepperoni!");
+    const attack =
+      this.objects[Object.keys(this.objects)[Math.floor(Math.random() * 2)]];
+    console.log(attack);
+    if (attack === 5) {
+      enemy.health -= 5;
+      console.log(
+        `DonutDougie got hit by pepperoniStars! His health is now at ${enemy.health}`
+      );
+    } else if (attack === 10) {
+      enemy.health -= 10;
+      console.log(
+        `PizzaRat got hit by cheeseGrease! His health is now at ${enemy.health}`
+      );
+    }
   }
 }
 const pizza = new Enemy("Pizza Rat");
+
+donut.talkSass();
+pizza.talkSmack();
+donut.announceHealth();
+pizza.announceHealth();
+donut.battle(pizza);
+pizza.battle(donut);
+donut.announceHealth();
+pizza.announceHealth();
