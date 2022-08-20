157a8fe5acdeed8e993b0624714b8d4e7bd066e8
imamyy <amytran2608@gmail.com>
2022-08-20 01:41:01 -0400

    Created Person class

--- objects.js
+++ objects.js
@@ -14,3 +14,45 @@ class Hamster {
     return this.price;
   }
 }
+class Person {
+  constructor(name, age, height, weight, mood, hamsters, bankAccount) {
+    this.name = name;
+    this.age = 0;
+    this.height = 0;
+    this.weight = 0;
+    this.mood = 0;
+    this.hamsters = [];
+    this.bankAccount = 0;
+  }
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return this.age;
+  }
+  getWeight() {
+    return this.weight;
+  }
+  greet() {
+    console.log(`Hello! My name is ${this.name}`);
+  }
+  eat() {
+    this.weight++;
+    this.mood++;
+  }
+  exercise() {
+    weight--;
+  }
+  ageUp() {
+    this.age++;
+    this.height++;
+    this.weight++;
+    this.mood--;
+    this.bankAccount += 10;
+  }
+  buyHamster(hamster) {
+    this.hamsters.push(hamster);
+    this.mood += 10;
+    this.bankAccount -= getPrice();
+  }
+}
