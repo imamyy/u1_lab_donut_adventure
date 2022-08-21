3984589c27a114d1406bc6a786e93dfc26f856c2
imamyy <amytran2608@gmail.com>
2022-08-21 01:30:54 -0400

    Added parent class

--- adventure.js
+++ adventure.js
@@ -1,7 +1,16 @@
-class Hero {
+class Denizen {
   constructor(name) {
     this.name = name;
     this.health = 100;
+  }
+  announceHealth() {
+    console.log(this.health);
+  }
+}
+
+class Hero extends Denizen {
+  constructor(name) {
+    super(name);
     this.objects = {
       sprinkleSpray: 5,
       sugarShock: 10,
@@ -14,33 +23,21 @@ class Hero {
   talkSass() {
     console.log(this.catchPhrases[Math.floor(Math.random() * 2)]);
   }
-  announceHealth() {
-    console.log(this.health);
-  }
   battle(enemy) {
-    console.log("I'm ready to rumble");
-    const attack =
-      this.objects[Object.keys(this.objects)[Math.floor(Math.random() * 2)]];
-    console.log(attack);
-    if (attack === 5) {
-      enemy.health -= 5;
-      console.log(
-        `PizzaRat got hit by sprinkleSpray! His health is now at ${enemy.health}`
-      );
-    } else if (attack === 10) {
-      enemy.health -= 10;
-      console.log(
-        `PizzaRat got hit by sugarShock! His health is now at ${enemy.health}`
-      );
-    }
+    let attack = Object.keys(this.objects)[Math.floor(Math.random() * 2)];
+    let hitpoint = this.objects[attack];
+    console.log(attack + ": " + hitpoint);
+    enemy.health -= hitpoint;
+    console.log(
+      `${enemy.name} got hit by ${attack}! His health is now at ${enemy.health}`
+    );
   }
 }
 const donut = new Hero("Dougie");
 
-class Enemy {
+class Enemy extends Denizen {
   constructor(name) {
-    this.name = name;
-    this.health = 100;
+    super(name);
     this.objects = {
       pepperoniStars: 5,
       cheeseGrease: 10,
@@ -53,34 +50,29 @@ class Enemy {
   talkSmack() {
     console.log(this.catchPhrases[Math.floor(Math.random() * 2)]);
   }
-  announceHealth() {
-    console.log(this.health);
-  }
   battle(enemy) {
-    console.log("i'm gonna flatten you like a slice of pepperoni!");
-    const attack =
-      this.objects[Object.keys(this.objects)[Math.floor(Math.random() * 2)]];
-    console.log(attack);
-    if (attack === 5) {
-      enemy.health -= 5;
-      console.log(
-        `DonutDougie got hit by pepperoniStars! His health is now at ${enemy.health}`
-      );
-    } else if (attack === 10) {
-      enemy.health -= 10;
-      console.log(
-        `PizzaRat got hit by cheeseGrease! His health is now at ${enemy.health}`
-      );
-    }
+    let attack = Object.keys(this.objects)[Math.floor(Math.random() * 2)];
+    let hitpoint = this.objects[attack];
+    console.log(attack + ": " + hitpoint);
+    enemy.health -= hitpoint;
+    console.log(
+      `${enemy.name} got hit by ${attack}! His health is now at ${enemy.health}`
+    );
   }
 }
 const pizza = new Enemy("Pizza Rat");
 
-donut.talkSass();
-pizza.talkSmack();
-donut.announceHealth();
-pizza.announceHealth();
-donut.battle(pizza);
-pizza.battle(donut);
-donut.announceHealth();
-pizza.announceHealth();
+while (true) {
+  donut.talkSass();
+  pizza.talkSmack();
+  donut.battle(pizza);
+  if (pizza.health <= 0) {
+    console.log("Donut is the winner!");
+    break;
+  }
+  pizza.battle(donut);
+  if (donut.health <= 0) {
+    console.log("Pizza is the winner");
+    break;
+  }
+}
